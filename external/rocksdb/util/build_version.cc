1f278aa1edb4f98ed5634a51965a21336b771975
Fedor Buzinov <fedor@prediapp.com>
2018-01-18 20:54:45 +0300

    Update

--- external/rocksdb/util/build_version.cc
+++ external/rocksdb/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:ec5d900c2601a44b0f82a30c8eaa0bd419437d45";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:d8094a0469bce5b11c55f890d9373394bcfefab5";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2018-01-18";
 const char* rocksdb_build_compile_date = __DATE__;
